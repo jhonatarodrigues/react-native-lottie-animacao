3968ee12165a6c487af735ef0d59abc00463df28
jhonata rodrigues <jhonata.a.r@hotmail.com>
2020-07-28 00:01:37 -0300

    animacao do paraquedas

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+// -- animacao
+import paraquedas from './src/json/paraquedasCaixa.json';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Teste de animação com lottie</Text>
+      <View style={styles.viewAnimacao}>
+        <LottieView resizeMode="contain" source={paraquedas} autoPlay loop />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  viewAnimacao: {
+    width: 400,
+    height: 400,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
