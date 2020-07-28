c41c1710f4152b2d3716dd17ee896e7ca9f09210
jhonata rodrigues <jhonata.a.r@hotmail.com>
2020-07-28 00:01:49 -0300

    code review

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
 import LottieView from 'lottie-react-native';
